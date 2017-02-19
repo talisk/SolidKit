52730383292c2da82c77d470956b5137ab057d81
talisk <talisk@talisk.cn>
2017-02-20 00:27:49 +0800

    Add test case

--- SolidKitExample/SolidKitExample/ViewController.m
+++ SolidKitExample/SolidKitExample/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "View.h"
+#import "SolidKit.h"
 
 @interface ViewController ()
 
@@ -16,9 +18,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    View *view = [[View alloc] init];
+    [self.view addSubview:view];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)query:(id)sender {
+//    [SKDatabaseManager selectDataWithLimit:3 completionHandler:^(NSArray *array) {
+//        NSLog(@"query\n%@", array);
+//    }];
+    [SKDatabaseManager selectAllWithCompletionHandler:^(NSArray *result) {
+        NSLog(@"query\n%@", result);
+    }];
+}
 
+- (IBAction)pressed:(id)sender {
+    NSDictionary *dic = @{@"heheh": @[@1, @2],
+                          @"fdsafsfas": @"fffff",
+                          @"sdfas": @1,
+                          @"dictionary": @{@"dic1": @"111", @"dic2": @222}
+                          };
+    
+    [SKDatabaseManager insertData:dic completionHandler:^{
+        Debug(@"%@", dic);
+//        Info(@"%@", dic);
+//        Warning(1, @"%@", dic);
+//        Error(1, @"%@", dic);
+    }];
+//    [SKDatabaseManager deleteDataCount:3 completionHandler:^{
+//        NSLog(@"delete3");
+//    }];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
