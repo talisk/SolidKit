55a69455a9f780433ef25e10d8f841bf358b35fe
talisk <talisk@talisk.cn>
2017-03-04 16:32:13 +0800

    Add performance log class

---
+++ SolidKitExample/SolidKit/SKPerformanceLog.h
@@ -0,0 +1,19 @@
+//
+//  SKPerformanceLog.h
+//  SolidKitExample
+//
+//  Created by 孙恺 on 2017/3/4.
+//  Copyright © 2017年 talisk. All rights reserved.
+//
+
+#import "SKLog.m"
+
+typedef NS_ENUM(NSInteger, SKPerformanceLogType) {
+    SKPerformanceLogPerformanceTypeMemoryUsage = 1,
+    SKPerformanceLogPerformanceTypeCPUUsage = 2,
+    SKPerformanceLogPerformanceTypeSmooth = 3,
+    SKPerformanceLogPerformanceTypeBattery = 4,
+};
+
+extern void __SKPerformanceLog(SKPerformanceLogType performance_type,
+                               NSString *format, ...) NS_FORMAT_FUNCTION(2, 3);
---
+++ SolidKitExample/SolidKit/SKPerformanceLog.m
@@ -0,0 +1,44 @@
+//
+//  SKPerformanceLog.m
+//  SolidKitExample
+//
+//  Created by 孙恺 on 2017/3/4.
+//  Copyright © 2017年 talisk. All rights reserved.
+//
+
+#import "SKPerformanceLog.h"
+
+@interface SKPerformanceLog : SKLog
+
+@end
+
+@implementation SKPerformanceLog
+
+@end
+
+void __SKPerformanceLog(SKPerformanceLogType performance_type,
+                        NSString *format, ...) {
+    @autoreleasepool {
+        va_list args;
+        va_start(args, format);
+        NSString *user_msg = [[NSString alloc] initWithFormat:format arguments:args];
+        va_end(args);
+        
+        dispatch_async(log_queue(), ^{
+            
+                struct timeval tv;
+                gettimeofday(&tv , NULL);
+                
+                log_info("%ld%d | %s", tv.tv_sec, tv.tv_usec/1000, [user_msg UTF8String]);
+                
+                [SKDatabaseManager insertDictionary:@{
+                                                      @"timestamp": [[NSString alloc] initWithFormat:@"%ld.%d", tv.tv_sec, tv.tv_usec],
+                                                      @"type": [[NSString alloc] initWithFormat:@"%ld", performance_type],
+                                                      @"value": [[NSString alloc] initWithFormat:@"%@", user_msg]
+                                                      } type:SKDataTypeLog completionHandler:^{
+                                                          
+                                                      }];
+            
+        });
+    }
+}
